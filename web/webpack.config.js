ff0315101be43ffbfd603a2d2ececdc95d474cd8
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2021-06-20 15:34:26 +0700

    feat: setup react project

---
+++ web/webpack.config.js
@@ -0,0 +1,45 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const commonConfig = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)/i,
+        exclude: path.resolve(__dirname, "node_modules"),
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  target: "web",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+};
+
+const developmentConfig = merge(commonConfig, {
+  mode: "development",
+  devtool: "source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname, "build"),
+    open: true,
+  },
+});
+
+const productionConfig = merge(commonConfig, { mode: "production" });
+
+module.exports = (_env, argv) =>
+  argv.mode === "development" ? developmentConfig : productionConfig;
